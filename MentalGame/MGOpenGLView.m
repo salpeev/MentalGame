6cd4fe1a2993e822abaa4279f62ec19ab3914150
Sergey Alpeev <salpeev@Mac-mini-Sergey.local>
2013-09-08 00:30:29 +0300

    MGOpenGLView with multisample

--- MentalGame/MGOpenGLView.m
+++ MentalGame/MGOpenGLView.m
@@ -13,6 +13,13 @@
 
 
 @interface MGOpenGLView ()
+{
+    GLuint m_resolveFramebuffer;
+    GLuint m_sampleFramebuffer;
+    GLuint m_resolveColorRenderbuffer;
+    GLuint m_sampleColorRenderbuffer;
+    GLuint m_sampleDepthStencilRenderbuffer;
+}
 
 @property (strong, nonatomic) EAGLContext *eaglContext;
 
@@ -24,12 +31,52 @@
 
 @implementation MGOpenGLView
 
+#pragma mark - Class Methods
+
++ (Class)layerClass
+{
+    return [CAEAGLLayer class];
+}
+
 #pragma mark - Lifecycle
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
+        CGFloat scale = [UIScreen mainScreen].scale;
+        CGFloat width = CGRectGetWidth(frame) * scale;
+        CGFloat height = CGRectGetHeight(frame) * scale;
+        
+        CAEAGLLayer *eaglLayer = (CAEAGLLayer *)self.layer;
+        eaglLayer.opaque = YES;
+        eaglLayer.contentsScale = scale;
+        
+        self.eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+        [EAGLContext setCurrentContext:self.eaglContext];
+        
+        glGenRenderbuffers(1, &m_resolveColorRenderbuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, m_resolveColorRenderbuffer);
+        [self.eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer];
+        
+        glGenFramebuffers(1, &m_resolveFramebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, m_resolveFramebuffer);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_resolveColorRenderbuffer);
+        
+        glGenRenderbuffers(1, &m_sampleColorRenderbuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, m_sampleColorRenderbuffer);
+        glRenderbufferStorageMultisampleAPPLE(GL_RENDERBUFFER, 4, GL_RGBA8_OES, width, height);
+        
+        glGenRenderbuffers(1, &m_sampleDepthStencilRenderbuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, m_sampleDepthStencilRenderbuffer);
+        glRenderbufferStorageMultisampleAPPLE(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8_OES, width, height);
+        
+        glGenFramebuffers(1, &m_sampleFramebuffer);
+        glBindFramebuffer(GL_FRAMEBUFFER, m_sampleFramebuffer);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_sampleColorRenderbuffer);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_sampleDepthStencilRenderbuffer);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_sampleDepthStencilRenderbuffer);
+        
         CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(draw:)];
         [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     }
@@ -40,7 +87,28 @@
 
 - (void)draw:(CADisplayLink *)displayLink
 {
-    NSLog(@"%@", displayLink);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_sampleFramebuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_sampleColorRenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_sampleDepthStencilRenderbuffer);
+    
+    // TODO: Drawing code here
+    
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER_APPLE, m_resolveFramebuffer);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER_APPLE, m_sampleFramebuffer);
+    glResolveMultisampleFramebufferAPPLE();
+    
+    const GLenum discards[] =
+    {
+        GL_COLOR_ATTACHMENT0,
+        GL_DEPTH_ATTACHMENT,
+        GL_STENCIL_ATTACHMENT
+    };
+    glDiscardFramebufferEXT(GL_READ_FRAMEBUFFER_APPLE, 3, discards);
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, m_resolveFramebuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, m_resolveColorRenderbuffer);
+    
+    [self.eaglContext presentRenderbuffer:GL_RENDERBUFFER];
 }
 
 @end
