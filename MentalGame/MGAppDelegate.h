bf3f3e8d1ddd83d598c06b02d7776865c29e8187
Sergey Alpeev <salpeev@Mac-mini-Sergey.local>
2013-09-07 16:54:04 +0300

    Initial commit

---
+++ MentalGame/MGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MGAppDelegate.h
+//  MentalGame
+//
+//  Created by Sergey Alpeev on 07.09.13.
+//  Copyright (c) 2013 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MentalGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MentalGame
+//
+//  Created by Sergey Alpeev on 07.09.13.
+//  Copyright (c) 2013 Sergey Alpeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MGAppDelegate class]));
+    }
+}
